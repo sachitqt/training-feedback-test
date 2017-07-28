6b780cae2ee3755f2baa597fae7fdbce2bd03a92
lipikagupta-quovantis <lipika.gupta@quovantis.com>
2017-07-28 16:20:10 +0530

    Updated save user method. Added method to save and fetch user session.

--- TrainingFeedback/firebase_operations.js
+++ TrainingFeedback/firebase_operations.js
@@ -1,5 +1,6 @@
 let firebase = require('firebase')
 let exceltojson = require("xls-to-json-lc");
+let xlsxtojson = require("xlsx-to-json-lc");
 let i18n = require("i18n");
 
 i18n.configure({
@@ -22,34 +23,54 @@ firebase.initializeApp(config);
 
 module.exports = {
     saveQuestionsToDB: function (questions) {
-        writeQuestionsToFirebase(questions);
+        questions.forEach(function (question) {
+            writeQuestionData(question.id, question.question, question.options, question.question_type);
+        });
     },
     saveFeedbackToDB: function (trainingId, userId, questionAnswers) {
-        saveFeedback(trainingId, userId, questionAnswers);
+        getEmailIdFromUsername(userId, function (emailId) {
+            if (emailId) {
+                saveTrainingFeedback(trainingId, emailId, questionAnswers);
+            }
+        });
     },
     getUserEmailId: function (username, callbackFunction) {
         getEmailIdFromUsername(username, callbackFunction);
     },
-    isFeedbackPendingForUser: function (username, callbackFunction){
+    isFeedbackPendingForUser: function (username, callbackFunction) {
         getEmailIdFromUsername(username, function (emailId) {
             if (emailId) {
                 isFeedbackPending(emailId, callbackFunction);
             }
         });
     },
-    getPendingFeedbackForUser: function (username, callbackFunction){
+    getPendingFeedbackForUser: function (username, callbackFunction) {
         getEmailIdFromUsername(username, function (emailId) {
             if (emailId) {
                 getUserPendingFeedback(emailId, callbackFunction);
             }
         });
     },
-    deleteUserPendingFeedback : function (trainingId, username) {
+    deleteUserPendingFeedback: function (trainingId, username) {
         getEmailIdFromUsername(username, function (emailId) {
             if (emailId) {
                 deletePendingFeedback(trainingId, emailId);
             }
         });
+    },
+    addUserSession: function (username, address, skypeId) {
+        firebase.database().ref('sessions/').push({
+            username: username,
+            address: address,
+            skypeId: skypeId
+        });
+    },
+    getUserSession: function (callbackFunction) {
+        firebase.database().ref('session/').once('value', function (snapshot) {
+            callbackFunction(snapshot);
+        }, function (errorObject) {
+            console.log('The read failed: ' + errorObject.code);
+        })
     }
 };
 
@@ -79,8 +100,8 @@ function writeQuestionData(questionId, question, answerOptions, questionType) {
 }
 
 function saveUserData() {
-    exceltojson({
-        input: __dirname + '/user_data.xls',
+    xlsxtojson({
+        input: __dirname + '/user_data.xlsx',
         output: null,
         lowerCaseHeaders: false //to convert all excel headers to lowr case in json
     }, function (err, result) {
@@ -95,17 +116,31 @@ function saveUserData() {
 }
 
 function saveUser(userData) {
-    firebase.database().ref('users/' + userData.EmailId.replaceAll('.', ':')).set({
+    // firebase.database().ref('users/' + userData.EmailId.replaceAll('.', ':')).set({
+    //     employeeId: userData.EmployeeID,
+    //     firstName: userData.FirstName,
+    //     lastName: userData.LastName,
+    //     fullName: userData.FirstName + ' ' + userData.LastName,
+    //     emailId: userData.EmailId,
+    //     skypeId: userData.SkypeId
+    // });
+    // console.log(userData);
+    firebase.database().ref('users/' + userData.EmailID.replaceAll('.', ':')).set({
         employeeId: userData.EmployeeID,
         firstName: userData.FirstName,
         lastName: userData.LastName,
         fullName: userData.FirstName + ' ' + userData.LastName,
-        emailId: userData.EmailId,
-        skypeId: userData.SkypeId
+        emailId: userData.EmailID,
+        skypeId: userData.SkypeID,
+        title: userData.Title,
+        currentProductTeam: userData.CurrentProductTeam,
+        primaryReviewer: userData.PrimaryReviewer,
+        practiceGroup: userData.PracticeGroup,
+        practiceHead: userData.PracticeHead
     });
 }
 
-function saveFeedback(trainingId, userId, questionAnswers){
+function saveFeedback(trainingId, userId, questionAnswers) {
     getEmailIdFromUsername(userId, function (emailId) {
         if (emailId) {
             saveTrainingFeedback(trainingId, emailId, questionAnswers);
@@ -159,9 +194,20 @@ function saveTrainingData(trainingName, facilitator, attendees, trainingDate) {
 function fetchTrainingData() {
     firebase.database().ref('trainings/').once('value', function (snapshot) {
         console.log(snapshot.val());
+        snapshot.forEach(function (user) {
+            console.log(user.val());
+        });
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
     });
+}
+
+function getSelectedTraining(trainingId, callbackFunction) {
+    firebase.database().ref('trainings/' + trainingId).once('value', function (snapshot) {
+        callbackFunction(snapshot);
+    }, function (errorObject) {
+        console.log('The read failed: ' + errorObject.code);
+    })
 }
 
 function fetchNonFilledTrainings() {
@@ -197,19 +243,19 @@ function getEmailIdFromUsername(username, callbackFunction) {
     })
 }
 
-function getUserPendingFeedback(emailId, callbackFunction){
-    firebase.database().ref('pendingFeedback/'+emailId.replaceAll('.', ':')).once('value', function (snapshot) {
+function getUserPendingFeedback(emailId, callbackFunction) {
+    firebase.database().ref('pendingFeedback/' + emailId.replaceAll('.', ':')).once('value', function (snapshot) {
         callbackFunction(snapshot);
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
     })
 }
 
-function deletePendingFeedback(trainingId, emailId){
+function deletePendingFeedback(trainingId, emailId) {
     firebase.database().ref('pendingFeedback/' + emailId.replaceAll('.', ':') + '/' + trainingId).remove();
 }
 
-function getQuestions(){
+function getQuestions() {
     firebase.database().ref('questions/').once('value', function (snapshot) {
         console.log(snapshot);
     }, function (errorObject) {
