9b333ef90ab282da64202a2651e30038ce9f0619
sachit <sachit.wadhawan@quovantis.com>
2017-11-29 12:53:17 +0530

    Changes in env file

--- bot.js
+++ bot.js
@@ -17,6 +17,7 @@ i18n.configure({
     directory: __dirname + '/locales'
 });
 
+// here the credentials will be fetched from env file
 const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
